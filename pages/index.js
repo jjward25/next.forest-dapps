489c52f022cbe5135fc5105e8e074b241a9ae902
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-04-09 11:32:22 -0600

    textUpdate

--- pages/index.js
+++ pages/index.js
@@ -51,18 +51,18 @@ export default function HomeContent() {
             <div className={styles["summary-blurb-header"]}>The Dapp Store</div>
             <li className={styles["summary-blurb-text"]}>
               The value of a crypto asset is largely based on adoption, not
-              traditional financial metrics, so we provide usage-focused charts
-              with insight into how those metrics are typically interpreted.
+              traditional quarterly metrics, so we provide usage-focused charts
+              with notes into how those metrics can be interpreted.
             </li>
             <li className={styles["summary-blurb-text"]}>
-              Our “Dapp Store” layout provides the perfect context for newcomers
-              to evaluate projects as real-world solutions.
+              Our “Dapp Store” layout provides the perfect user-friendly context
+              for newcomers to evaluate Web3 technology as a real-world
+              solution.
             </li>
             <li className={styles["summary-blurb-text"]}>
-              Our goal is to maximize knowledge to minimize risk... to give you
-              all the information you need as an individual or business manager
-              to make the right call on Web3 vendors and early-stage
-              investments.
+              Our goal is to maximize knowledge to minimize risk... to give
+              users context as an individual or business manager to make the
+              right call on Web3 vendors and early-stage investments.
             </li>
           </div>
         </div>
@@ -78,7 +78,7 @@ export default function HomeContent() {
             >{`Well, actually those are more like traditional databases.  Blockchains are more like if 10,000 receipt-checkers all reviewed your receipt, and checked your purchase against the store inventory (to make sure they had the goods to sell) AND your bank accounts (to make sure you had the money).`}</div>
             <div
               className={styles["long-form-text"]}
-            >{`That whole process of handling the transaction and verifying the truthfulness of that transaction is what blockchains do. And they do it by storing the history in multiple places, so you'd have to corrupt 5,001 receipt-checkers to change the record illicitly.`}</div>
+            >{`That whole process of handling the transaction and verifying the 'truthfulness' of that transaction is what blockchains do. And they do it by storing the history in multiple places, so you'd have to corrupt 5,001 receipt-checkers to change the record illicitly.`}</div>
             <div
               className={styles["long-form-text"]}
             >{`This type of network security is a form of cryptography, thus the 'crypto' name.`}</div>
@@ -103,10 +103,10 @@ export default function HomeContent() {
             <div className={styles["long-form-header"]}>What We Do</div>
             <div
               className={styles["long-form-text"]}
-            >{`Here at The Forest, we help you make sense of everything we summarized up above by making sense of Dapps. Dapps are “Decentralized Apps,” meaning they use a blockchain to handle at least some interactions.`}</div>
+            >{`We help you make sense of everything by making sense of Dapps. Dapps are “Decentralized Apps,” which in practice means they use a blockchain to handle at least some interactions or data.`}</div>
             <div
               className={styles["long-form-text"]}
-            >{`More specifically, decentralized means that the users own the service.  Whoever owns the token gets to vote on updates and changes to the service, and the dedicated team (who receives pre-determined salaries from the network) will implement the changes.`}</div>
+            >{`More generally, decentralized means that the users own the service.  Whoever owns the token gets to vote on updates and changes to the service, and the dedicated team (who receives pre-determined salaries from the network) will implement the changes.`}</div>
             <div
               className={styles["long-form-text"]}
             >{`Since a Dapp’s popularity is what drives its token’s value, usage metrics are crucial to evaluating the quality and potential of a service.  Is a network’s transaction volume mostly speculative investors, or users interacting with the service?  Is it a lot of users or a few power users? These tell us things about a service’s usefulness, and thus its value.`}</div>
@@ -118,16 +118,14 @@ export default function HomeContent() {
             <div className={styles["long-form-header"]}>Our Approach</div>
             <div
               className={styles["long-form-text"]}
-            >{`We organize these new blockchain services like an app store, so you have better context of what they’re trying to do. We use a mix of usage based metrics and industry favored metrics to give you a picture of this service as a competitor in the category, as well as the way the market may view it as an investment.`}</div>
+            >{`Everybody's used an app store, so we organized blockchain services like an app store, so you have better context of what they’re trying to do. We provide a mix of usage based metrics and industry favored metrics to give you a picture of this service as a competitor in the category, as well as the way the market may view it as an investment.`}</div>
             <div
               className={styles["long-form-text"]}
-            >{`We also make it as easy as possible for you to get involved in the industry and start earning, from any device.  The risk to you is your initial investment.  Some blockchain networks are relatively mature, but most are not and could go to zero.`}</div>
+            >{`We also want to make it as easy as possible for you to get involved.  Whether you're a business manager looking for new tools, tech-forward users, or investors... our product pages will highlight white papers, business development contacts, sign up pages, staking pools, and more.`}</div>
+
             <div
               className={styles["long-form-text"]}
-            >{`That said, the returns can be well worth that risk.  We want to help you make responsible investments in high-potential projects and get the most out of this emerging tech.`}</div>
-            <div
-              className={styles["long-form-text"]}
-            >{`The future can be a scary place, but it doesn’t have to be. Start staking for yourself today.`}</div>
+            >{`The future can be a scary place, but it doesn’t have to be. Let us show you around.`}</div>
           </div>
         </div>
       </main>

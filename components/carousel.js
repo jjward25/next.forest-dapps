2b84f4d43e068aba69018db03e20174d7c3e7fb7
jjward25 <45015765+jjward25@users.noreply.github.com>
2022-04-09 12:17:13 -0600

    carouselButtons

--- components/carousel.js
+++ components/carousel.js
@@ -1,14 +1,28 @@
 import styles from "./Carousel.module.scss";
 import DappCard from "./dappcard";
+import { useRef } from "react";
 export default function Carousel(props) {
+  const ref = useRef(null);
+  const scroll = (scrollOffset) => {
+    ref.current.scrollLeft += scrollOffset;
+  };
   return (
     <div className={styles["single-carousel"]}>
       <div className={styles["carousel-header"]}>{props.Content.Category}</div>
+      <div className={styles["carousel-description"]}>
+        {props.Content.CategoryDescription}
+      </div>
 
-      <div className={styles["carousel-cards-wrap"]}>
-        {props.Content.DappList.map((dapp) => {
-          return <DappCard dapp={dapp} key={dapp.Provider} />;
-        })}
+      <div className={styles["carousel-wrap"]}>
+        <div className={styles["carousel-cards-wrap"]} ref={ref}>
+          {props.Content.DappList.map((dapp) => {
+            return <DappCard dapp={dapp} key={dapp.Provider} />;
+          })}
+        </div>
+        <button
+          className={styles["scroll-button"]}
+          onClick={() => scroll(675)}
+        >{`>`}</button>
       </div>
     </div>
   );
